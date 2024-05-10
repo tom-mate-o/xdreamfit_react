d94489eaa1676cb672c0640321b164db74df8ab1
tom-mate-o <132936694+tom-mate-o@users.noreply.github.com>
2024-05-10 09:46:30 +0200

    icons slider

--- src/components/PauseOnHover.jsx
+++ src/components/PauseOnHover.jsx
@@ -3,6 +3,14 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import healthicon from '../../src/img/healthicon.png';
+import air from '../../src/img/air.png';
+import apnoe from '../../src/img/apnoe.png';
+import aroma from '../../src/img/aroma.png';
+import fitness from '../../src/img/fitness.png';
+import forest from '../../src/img/forest.png';
+import ice from '../../src/img/ice.png';
+import mindful from '../../src/img/mindful.png';
+import yogamat from '../../src/img/yoga-mat.png';
 
 export default function PauseOnHover() {
   var settings = {
@@ -36,7 +44,7 @@ export default function PauseOnHover() {
       <Slider {...settings}>
         <div className="info__slide" id="1">
           <div className="info__card">
-            <img src={healthicon} alt="icon"></img>
+            <img src={fitness} alt="icon"></img>
             <h3>
               Personal <br />
               Fitness <br />
@@ -46,7 +54,7 @@ export default function PauseOnHover() {
         </div>
         <div className="info__slide" id="2">
           <div className="info__card">
-            <img src={healthicon} alt="icon"></img>
+            <img src={yogamat} alt="icon"></img>
             <h3>
               Pilates-
               <br />
@@ -57,19 +65,19 @@ export default function PauseOnHover() {
         </div>
         <div className="info__slide" id="3">
           <div className="info__card">
-            <img src={healthicon} alt="icon"></img>
+            <img src={air} alt="icon"></img>
             <h3>Atemtraining</h3>
           </div>
         </div>
         <div className="info__slide" id="4">
           <div className="info__card">
-            <img src={healthicon} alt="icon"></img>
+            <img src={forest} alt="icon"></img>
             <h3>Outdoortraining</h3>
           </div>
         </div>
         <div className="info__slide" id="5">
           <div className="info__card">
-            <img src={healthicon} alt="icon"></img>
+            <img src={mindful} alt="icon"></img>
             <h3>
               Entspannungs- <br />
               und
@@ -80,13 +88,13 @@ export default function PauseOnHover() {
         </div>
         <div className="info__slide" id="6">
           <div className="info__card">
-            <img src={healthicon} alt="icon"></img>
+            <img src={apnoe} alt="icon"></img>
             <h3>Apnoetauchkurse</h3>
           </div>
         </div>
         <div className="info__slide" id="7">
           <div className="info__card">
-            <img src={healthicon} alt="icon"></img>
+            <img src={ice} alt="icon"></img>
             <h3>
               Eisbaden <br />
               Eisschwimmen <br />
@@ -96,7 +104,7 @@ export default function PauseOnHover() {
         </div>
         <div className="info__slide" id="8">
           <div className="info__card">
-            <img src={healthicon} alt="icon"></img>
+            <img src={aroma} alt="icon"></img>
             <h3>
               Aromatherapie <br />& Energetische <br />
               Körperanwendungen
